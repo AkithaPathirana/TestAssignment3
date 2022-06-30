03725fb934298d015a55f218702c54ecf57c0f86
AkithaPathirana <73589451+AkithaPathirana@users.noreply.github.com>
2022-06-30 13:00:43 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;  // Import the Scanner class
 
-class Main {
+class hello {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
     System.out.println("Enter name");
